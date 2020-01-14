db844183c45401a1dc47aa558774bfebc9c4ed6d
aaronchiu0 <48235261+aaronchiu0@users.noreply.github.com>
2020-01-14 01:39:27 -0500

    update

--- _site/assets/js/display.js
+++ _site/assets/js/display.js
@@ -1,23 +1,55 @@
+// for viewport height
 let vh = window.innerHeight * 0.01;
 
-$(window).scroll(function() {
-    var y = $(this).scrollTop();
-    $(".hero-title-container").css("opacity", 1 - y / 250);
+// bounding function 
+function clamp(num, min, max) {
+    return num <= min ? min : num >= max ? max : num;
+}
+
+$(document).ready(function(){
+    $(window).scroll(function() {
+        var y = $(this).scrollTop();    // distance from top of page in px
+        var opacity = 1 - y / 250;      // opacity lowering calculation
+        $(".hero-title-container").css({
+            "opacity": clamp(opacity, 0, 1)
+        });
+    });
 });
 
 /* $(window).scroll(function() {
     var y = $(this).scrollTop();
     console.log(y);
-    if (y > vh*210 && y < vh*320) {
-        $(".skills-text-container").css("opacity", (y-vh*210)*1/250);
+    if (y < vh*320) {
+        $(".skills-container").css("position", "sticky");
     }
     else if (y > vh*320) {
-        $(".skills-text-container").css("opacity", 1 - (y-vh*320)/250);
+        $(".skills-container").css("position", "relative");
+        $(".skills-container").css("bottom", 0);
     }
     else {
-        $(".skills-text-container").css("opacity", 0);
+        ;//$(".skills-container").css("opacity", 0);
     }
 }); */
+
+/* $.fn.followTo = function (pos) {
+    var $this = this,
+        $window = $(window);
+
+    $window.scroll(function (e) {
+        if ($window.scrollTop() > pos) {
+            $this.css({
+                position: 'relative',
+                top: pos
+            });
+        } else {
+            $this.css({
+                position: 'sticky',
+            });
+        }
+    });
+};
+
+$('.skills-container').followTo(vh*340); */
 
 /* $(document).scroll(function() {
     var y = $(this).scrollTop();
