bb92107084e2f803789060344ce5d3ab57ccdc0a
aaronchiu0 <48235261+aaronchiu0@users.noreply.github.com>
2020-02-23 01:39:52 -0500

    Economics Calculator
    finish funtionality

--- _site/assets/js/display.js
+++ _site/assets/js/display.js
@@ -1,5 +1,5 @@
 // for viewport height
-let vh = window.innerHeight * 0.01;
+let vh = window.minnerHeight * 0.01;
 
 // bounding function 
 function clamp(num, min, max) {
@@ -29,47 +29,3 @@ $(document).ready(function(){
             $("#hero-title").hide();
     });
 });
-
-/* $(window).scroll(function() {
-    var y = $(this).scrollTop();
-    console.log(y);
-    if (y < vh*320) {
-        $(".skills-container").css("position", "sticky");
-    }
-    else if (y > vh*320) {
-        $(".skills-container").css("position", "relative");
-        $(".skills-container").css("bottom", 0);
-    }
-    else {
-        ;//$(".skills-container").css("opacity", 0);
-    }
-}); */
-
-/* $.fn.followTo = function (pos) {
-    var $this = this,
-        $window = $(window);
-
-    $window.scroll(function (e) {
-        if ($window.scrollTop() > pos) {
-            $this.css({
-                position: 'relative',
-                top: pos
-            });
-        } else {
-            $this.css({
-                position: 'sticky',
-            });
-        }
-    });
-};
-
-$('.skills-container').followTo(vh*340); */
-
-/* $(document).scroll(function() {
-    var y = $(this).scrollTop();
-    if (y > vh * 100 && vh * 200 > y) {
-      $('.skills-text-container').fadeIn();
-    } else {
-      $('.skills-text-container').fadeOut();
-    }
-}); */
---
+++ _site/assets/js/economics.js
@@ -0,0 +1,125 @@
+// global variables
+var scope = {
+    i: -1,
+    n: -1
+};
+var selectedFactor = -1;
+var selectedCalc = "";
+
+// expressions to evaluate
+var expressions = [
+    fp = math.parse('(1+i)^n'),
+    pf = math.parse('1/(1+i)^n'),
+    af = math.parse('i/((1+i)^n-1)'),
+    ap = math.parse('i*(1+i)^n/((1+i)^n-1)'),
+    fa = math.parse('((1+i)^n-1)/i'),
+    pa = math.parse('((1+i)^n-1)/(i*(1+i)^n)'),
+    ag = math.parse('1/i-n/((1+i)^n-1)'),
+    pg = math.parse('((1+i)^n-i*n-1)/(i^2*(1+i)^n)'),
+];
+
+// compile expressions
+var compiled_expressions = new Array;
+for (let i = 0; i < expressions.length; i++) {
+    compiled_expressions.push(expressions[i].compile());
+}
+
+var derivative_i = new Array;
+for (let i = 0; i < expressions.length; i++) {
+    derivative_i.push(math.derivative(expressions[i], 'i'));
+}
+
+var derivative_n = new Array;
+for (let i = 0; i < expressions.length; i++) {
+    derivative_n.push(math.derivative(expressions[i], 'n'));
+}
+
+function NewtonRaphson(equals, x, nmax, eps, del) {
+    var n, fx, fp;
+
+    selectedCalc == "rate" ? scope.i = x : scope.n = x;
+    console.log(selectedFactor, equals, scope);
+
+    fx = compiled_expressions[selectedFactor].evaluate(scope) - equals;
+    console.log(0, x, fx);
+
+    for (n = 1; n <= nmax; n++) {
+        selectedCalc == "rate" ? fp = derivative_i[selectedFactor].evaluate(scope) : fp = derivative_n[selectedFactor].evaluate(scope);
+
+        if (Math.abs(fp) < del) {
+            console.log("small derivative")
+            return;
+        }
+
+        let d = fx/fp;
+        x = x - d;
+        selectedCalc == "rate" ? scope.i = x : scope.n = x;
+        fx = compiled_expressions[selectedFactor].evaluate(scope) - equals;
+
+        console.log(n, x, fx);
+
+        if (Math.abs(d) < eps) {
+            console.log("Convergence");
+            break;
+        }
+    }
+    return x;
+} 
+
+$(document).ready(function(){
+    $("#calculation-type .factor").click(function() {
+        selectedFactor = $(this).index(".factor");
+        $("#calculation-IO h3").text("Calculating "+$(this).text());
+        $("#calculation-IO .amount-label").text($(this).text().charAt(2));
+        $("#calculation-IO-interpolate .first-label").text($(this).text().charAt(0));
+        $("#calculation-IO-interpolate .second-label").text($(this).text().charAt(2));
+
+        console.log(selectedFactor, $(this).text());
+    });
+
+
+    $("#calculation-IO .submit").click(function() {
+        var amount = parseFloat($(".amount").val());
+
+        scope = {
+            i: parseFloat($("#rate").val()),
+            n: parseFloat($("#period").val())
+        };
+
+        var factor = compiled_expressions[selectedFactor].evaluate(scope);
+
+        console.log(amount, factor, scope);
+
+        var ans = amount*factor;
+        math.format(ans, {precision: 10});
+        $("#calculation-IO .output").text(ans);
+
+        NewtonRaphson(2, 100, 0.0000000001, 0.01);
+
+
+    });
+
+    $("#calculation-IO-interpolate #calcRate").click(function() {
+        $("#calculation-IO-interpolate h3").text("Calculating "+$(this).text());
+        $(".calculate-label").text("Period");
+        selectedCalc = "rate";
+    });
+
+    $("#calculation-IO-interpolate #calcPeriod").click(function() {
+        $("#calculation-IO-interpolate h3").text("Calculating "+$(this).text());
+        $(".calculate-label").text("Rate");
+        selectedCalc = "period";
+    });
+
+    $("#calculation-IO-interpolate .submit").click(function() {
+        var first = parseFloat($("#calculation-IO-interpolate .first-amount").val());
+        var second = parseFloat($("#calculation-IO-interpolate .second-amount").val());
+        var known = parseFloat($(".calculate").val());
+
+        selectedCalc == "rate" ? scope.n = known : scope.i = known;
+
+        var ans = NewtonRaphson(first/second, 2, 100, 0.0000000001, 0.01);
+        math.format(ans, {precision: 10});
+        $("#calculation-IO-interpolate .output").text(ans);
+    });
+});
