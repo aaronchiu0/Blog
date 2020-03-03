4a60d97a3f1701d68a524cfbc1a8ce44c3ce7050
aaronchiu0 <48235261+aaronchiu0@users.noreply.github.com>
2020-03-02 19:03:45 -0500

    update

--- _site/assets/js/display.js
+++ _site/assets/js/display.js
@@ -13,11 +13,8 @@ function clamp(num, min, max) {
 
 $(document).ready(function(){
     $(window).scroll(function() {
-        var y = $(this).scrollTop();    // distance from top of page in px
-        var opacity = clamp(1 - y / 250, 0, 1);      // opacity lowering calculation
-        $(".hero-title-container").css({
-            "opacity": opacity
-        });
+        var y = $(this).scrollTop();                // distance from top of page in px
+        var opacity = clamp(1 - y / 250, 0, 1);     // opacity lowering calculation
 
         if (opacity > 0) {
             $("#hero-title").show();
